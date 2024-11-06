9dd6bf02e31784aad2fabfdfc0c74027e59a1244
tenimba <tenimba.sangare@epietch.eu>
2024-11-06 01:40:21 +0100

    back init

---
+++ backend/routes/personRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const personController = require('../controllers/personController');
+
+router.post('/', personController.createPerson);
+router.get('/', personController.getPersons);
+
+module.exports = router;
