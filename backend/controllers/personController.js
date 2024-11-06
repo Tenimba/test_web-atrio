5d2be9ea3ca74c2be044b8d0434f5294c878e374
tenimba <tenimba.sangare@epietch.eu>
2024-11-06 01:42:05 +0100

    back init

--- backend/controllers/personController.js
+++ backend/controllers/personController.js
@@ -1,9 +1,13 @@
-const { Person } = require('../models');
+const { Person, Job } = require('../models');
 
 exports.createPerson = async (req, res) => {
   try {
     const { firstName, lastName, birthDate } = req.body;
     const age = new Date().getFullYear() - new Date(birthDate).getFullYear();
+    
+    if (age < 0 || age > 150) {
+      return res.status(400).json({ error: "L'age doit etre entre 0 et 150 ans" });
+    }
     
     const person = await Person.create({ firstName, lastName, birthDate });
     res.status(201).json(person);
@@ -11,3 +15,15 @@ exports.createPerson = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+exports.getPersons = async (req, res) => {
+  try {
+    const persons = await Person.findAll({
+      include: [{ model: Job }]
+    });
+    res.json(persons);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
